85144518fe132cb129149e06364ca082a9e02948
dstinn <moralesdustin24@gmail.com>
2023-03-12 16:55:43 +0800

    Added share functionality on dish 3 and 4 and fixed dish 3 and 4 layout

--- app/src/main/java/com/hfad/kusinarecipes/dish3.java
+++ app/src/main/java/com/hfad/kusinarecipes/dish3.java
@@ -2,13 +2,44 @@ package com.hfad.kusinarecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class dish3 extends AppCompatActivity {
 
+    public ImageView ShareRecipe;
+    private View divider;
+    private ScrollView scrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dish3);
+        scrollView = findViewById(R.id.scroll);
+        divider = findViewById(R.id.divider);
+
+        ShareRecipe = findViewById(R.id.shareBtn);
+        ShareRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String body = "Download this app";
+                intent.putExtra(Intent.EXTRA_TEXT, body);
+                startActivity(Intent.createChooser(intent,"Share using:"));
+            }
+        });
+    }
+
+    public void scrollToCom(View view) {
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.scrollTo(0, divider.getTop());
+            }
+        });
     }
 }
